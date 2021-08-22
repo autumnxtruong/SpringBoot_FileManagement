6d91bb929052d40c7ed94a36804b28c3b59dc7d5
autumnxtruong <autumnxtruong@gmail.com>
2021-08-22 08:26:20 -0700

    multiple file upload demo (no db)

---
+++ uploadMultipleFile_demo/src/main/java/com/example/uploadMultipleFile_demo/UploadMultipleFileDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.uploadMultipleFile_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadMultipleFileDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadMultipleFileDemoApplication.class, args);
+	}
+
+}
---
+++ uploadMultipleFile_demo/src/test/java/com/example/uploadMultipleFile_demo/UploadMultipleFileDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadMultipleFile_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadMultipleFileDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
